678c1cdf56c3dc8e2636086e98f99a9e3bac1984
liamcreed <90063109+liamcreed@users.noreply.github.com>
2023-06-27 16:02:30 +0200

    pedal

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,10 @@
 /* 1. define variables */
-var me = "l.j.a.creed";
-var place = "gmail.com";
+var email = "l.j.a.creed";
+var domain = "gmail.com";
 
 /* 2. find email link to replace */
-var elink = document.getElementById("email");
+var email_link = document.getElementById("email");
 
 /* 3. replace link href with variables  */
 var body = ""
-elink.href = `mailto:${me}@${place}?body=${body}`;
+email_link.href = `mailto:${email}@${domain}?body=${body}`;
